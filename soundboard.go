2097e5cd6ace0ab5d719965777c0b9bb8eb7dce7
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-05-11 18:22:22 +0100

    Update soundboard.go

--- soundboard.go
+++ soundboard.go
@@ -10,6 +10,24 @@ package main
 func main() {
 	a := app.New()
 	w := a.NewWindow("Go Soundboard")
+	
+	uri1, err1 := storage.Child(app.Storage().RootURI(), "settings.json")
+	if err1 != nil {
+		log.Println("tasks_main: error-1: ", err1)
+	} else {
+		log.Println("tasks_main: URI-2-String: ", uri1.String())
+	}
+
+	
+	taskapp := &taskApp{
+		app:             a,
+		win:             w,
+		settings_uri:    uri1,
+	}
+	
+	taskapp.readSettings(uri1)
+	
 	w.SetContent(taskapp.makeUI())
+	w.Resize(fyne.NewSize(400, 700))
 	w.ShowAndRun()
 }
