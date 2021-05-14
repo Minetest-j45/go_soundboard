1f207c61aa6e43b4b8d8201f1d8e8c1d8cd9d668
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-05-14 21:36:53 +0100

    stop crash

--- json.go
+++ json.go
@@ -9,30 +9,30 @@ import (
 )
 
 type Buttons struct {
-  Buttons []Button `json:"buttons"`
+    Buttons []Button `json:"buttons"`
 }
 
 type Button struct {
-  Name   string `json:"name"`
-  Type   string `json:"file"`
-  Number   int `json:"number"`
+    Name   string `json:"name"`
+    File   string `json:"file"`
+    Number   int `json:"number"`
 }
 
 func openJson(file string) {
-  jsonFile, err := os.Open(file)
-  if err != nil {
-    fmt.Println(err)
-  }
-  fmt.Println("Successfully Opened " + file)
-  defer jsonFile.Close()
+    jsonFile, err := os.Open(file)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println("Successfully Opened " + file)
+    defer jsonFile.Close()
   
-  byteValue, _ := ioutil.ReadAll(jsonFile)
+    byteValue, _ := ioutil.ReadAll(jsonFile)
   
-  var buttons Buttons
+    var buttons Buttons
   
-  json.Unmarshal(byteValue, &buttons)
+    json.Unmarshal(byteValue, &buttons)
   
-      for i := 0; i < len(buttons.Buttons); i++ {
+    for i := 0; i < len(buttons.Buttons); i++ {
         fmt.Println("Button name: " + buttons.Buttons[i].Name)
         fmt.Println("Button file: " + buttons.Buttons[i].File)
         fmt.Println("Button number: " + strconv.Itoa(buttons.Buttons[i].Number))
